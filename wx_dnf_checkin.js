9e9a431d69ac83da806bd72c158a83c8b5d3abe5
风之凌殇 <fzls.zju@gmail.com>
2020-12-04 16:46:28 +0800

    使用封装的函数重写微信脚本

--- wx_dnf_checkin.js
+++ wx_dnf_checkin.js
@@ -2,40 +2,19 @@ var common = require("_common.js")
 
 common.init("微信dnf签到活动自动化脚本")
 
-common.log("打开微信")
-launchApp("微信")
-common.sleep_default()
-
+common.launchPackage("打开微信", "com.tencent.mm")
 
 common.log("先确保跳到首页")
 for (let i = 0; i < 5; i++) {
-    click(50, 135)
-    sleep(1000)
+    common.click("点击左上角返回键", 50, 135)
 }
-
-common.log("点击 [微信] tab")
-click(140, 2075)
-common.sleep_default()
-
-common.log("双击跳到最上方")
-click(50, 135)
-click(50, 135)
-common.sleep_default()
-
-common.log("点击 [文件助手] 对话框")
-click(500, 370)
-common.sleep_default()
-
-common.log("点击 [签到页面] 聊天记录")
-click(450, 1870)
-common.sleep_default()
-
+common.click("点击 [微信] tab", 140, 2075)
+common.double_click("双击跳到最上方", 50, 135)
+common.click("点击 [文件助手] 对话框", 500, 370)
+common.click("点击 [签到页面] 聊天记录", 450, 1870)
 // 为了保险起见，多点几次
 for (let i = 0; i < 5; i++) {
-    common.log("点击 [签到] 按钮")
-    click(535, 1370)
-    common.sleep_default()
+    common.click(i + ". 点击 [签到] 按钮", 535, 1370)
 }
 
-// 运行完毕
-common.log("操作完成，请看看是否签到完成了")
+common.foot()
