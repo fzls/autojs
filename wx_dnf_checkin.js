c5d0e9d3a7db697e51d5caa86bc91aa6d6808471
风之凌殇 <fzls.zju@gmail.com>
2020-12-09 17:25:20 +0800

    微信部分点击改为根据文字判断

--- wx_dnf_checkin.js
+++ wx_dnf_checkin.js
@@ -10,8 +10,8 @@ for (let i = 0; i < 5; i++) {
 }
 common.click("点击 [微信] tab", 140, 2075)
 common.double_click("双击跳到最上方", 50, 135)
-common.click("点击 [文件助手] 对话框", 500, 370)
-common.click("点击 [签到页面] 聊天记录", 450, 1870)
+common.click_text("点击 [文件助手] 对话框", "文件传输助手")
+common.click_text("点击 [签到页面] 聊天记录", "每日签到")
 // 为了保险起见，多点几次
 for (let i = 0; i < 5; i++) {
     common.click(i + ". 点击 [签到] 按钮", 535, 1370)
