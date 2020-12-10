384f6798b8a387f1367f23d0de5eb26f8c7f58c8
风之凌殇 <fzls.zju@gmail.com>
2020-12-11 00:27:50 +0800

    新活动需要先回复盖楼-。-

--- wx_dnf_checkin.js
+++ wx_dnf_checkin.js
@@ -10,21 +10,32 @@ for (let i = 0; i < 5; i++) {
 }
 common.click("点击 [微信] tab", 140, 2075)
 common.double_click("双击跳到最上方", 50, 135)
-common.click_text("点击 [文件助手] 对话框", "文件传输助手")
 
 common.headline("每日签到")
+common.click_text("点击 [文件传输助手] 对话框", "文件传输助手")
 common.click_text("点击 [签到页面] 聊天记录", "每日签到")
 // 为了保险起见，多点几次
 for (let i = 0; i < 3; i++) {
     common.click("第" + i + "次点击 [签到] 按钮", 535, 1370)
 }
 common.back()
+common.back()
 
-common.headline("2020DNF嘉年华派送好礼（TODO：确认是否必须要先去公众号回复 盖楼 才能领取）")
+common.headline("2020DNF嘉年华派送好礼")
+common.click_text("点击 [地下城与勇士] 对话框", "地下城与勇士")
+common.click("点击 左下角的 [文字输入] 按钮", 66, 2080)
+// hack: 版本更新的时候需要调整这个id
+let inputArea = className("android.widget.EditText").id("al_").findOne()
+inputArea.setText("盖楼")
+common.sleep_default_with_msg("输入 盖楼")
+common.click("点击 [发送] 按钮", 980, 2080)
+common.back()
+common.click_text("点击 [文件传输助手] 对话框", "文件传输助手")
 common.click_text("点击 [领取奖励] 聊天记录", "领取奖励")
 // 为了保险起见，多点几次
 for (let i = 0; i < 3; i++) {
     common.click("第" + i + "次点击 [领取奖励] 按钮", 535, 1570)
+    common.click("第" + i + "次点击 [确定] 按钮", 535, 1235)
 }
 common.back()
 
