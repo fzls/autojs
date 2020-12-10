54436ae632fd78508406e4564accb63bb82d0fc9
风之凌殇 <fzls.zju@gmail.com>
2020-12-10 22:16:06 +0800

    增加新的微信签到活动：2020DNF嘉年华派送好礼

--- wx_dnf_checkin.js
+++ wx_dnf_checkin.js
@@ -11,10 +11,21 @@ for (let i = 0; i < 5; i++) {
 common.click("点击 [微信] tab", 140, 2075)
 common.double_click("双击跳到最上方", 50, 135)
 common.click_text("点击 [文件助手] 对话框", "文件传输助手")
+
+common.headline("每日签到")
 common.click_text("点击 [签到页面] 聊天记录", "每日签到")
 // 为了保险起见，多点几次
-for (let i = 0; i < 5; i++) {
-    common.click(i + ". 点击 [签到] 按钮", 535, 1370)
+for (let i = 0; i < 3; i++) {
+    common.click("第" + i + "次点击 [签到] 按钮", 535, 1370)
 }
+common.back()
+
+common.headline("2020DNF嘉年华派送好礼（TODO：确认是否必须要先去公众号回复 盖楼 才能领取）")
+common.click_text("点击 [领取奖励] 聊天记录", "领取奖励")
+// 为了保险起见，多点几次
+for (let i = 0; i < 3; i++) {
+    common.click("第" + i + "次点击 [领取奖励] 按钮", 535, 1570)
+}
+common.back()
 
 common.foot()
