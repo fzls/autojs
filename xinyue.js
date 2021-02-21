1bc019837558e19f72ca7e6a37e419263ab2acfe
风之凌殇 <fzls.zju@gmail.com>
2021-02-21 11:37:01 +0800

    入口又变回来了，重新启用

--- xinyue.js
+++ xinyue.js
@@ -84,239 +84,238 @@ if (currentGPoints >= needLeftAtLeast + 44) {
 
 common.back()
 
-// // re: 因为心悦猫咪入口变了，但是收益不是太大，又耗时，懒得接入新的入口，因此临时注释掉心悦猫咪相关操作
-// common.headline("心悦猫咪")
-//
-// common.headline("领取历练奖励")
-// common.click("点击 [心悦猫咪] 区域", 525, 910)
-// common.sleep("加载很慢，多等一段时间", 25)
-// common.click("点击 [领取礼包] 按钮", 170, 1725)
-// common.click("点击 [好的] 按钮", 550, 1380)
-// common.back()
-//
-// common.headline("尝试购买妆容和装饰")
-// common.click("点击 [心悦猫咪] 区域", 525, 910)
-// common.sleep("加载很慢，多等一段时间", 25)
-// common.click("点击 [商店] 按钮", 910, 1725)
-//
-// // 小橘子的七日收益率较高，故而先尝试购买小橘子
-// // 成本    300
-// // 收益    30*2*7=420
-// // 净利润  120
-// // 收益率  120/300 = 40%
-// common.click("点击 [装饰] 按钮", 790, 390)
-// decorationMap = {
-//     "喷香猫粮": "icon-00",
-//     "飞车酷比": "icon-01",
-//     "飞车墨汁": "icon-02",
-//     "炫舞粉66": "icon-03",
-//     "炫舞酷66": "icon-04",
-//     "云裳茶壶": "icon-05",
-//     "云裳童心饰": "icon-06",
-//     "小橘子": "icon-07",
-// }
-// // common.sleep_default_with_msg("目前G分不太够，先不购买300G分的小橘子。等俩月卡都买完再开启")
-// locateItemAndTryBuyIt("小橘子", decorationMap)
-//
-// // 贤德昭仪的七日收益率较低，故而在后面买
-// // 成本    600
-// // 收益    50*2*7=700
-// // 净利润  100
-// // 收益率  100/600 = 16.66%
-// common.click("点击 [妆容] 按钮", 290, 390)
-// makeupMap = {
-//     "富贵士绅": "icon-01",
-//     "加班女仆": "icon-02",
-//     "浪子刀客": "icon-03",
-//     "世家公子": "icon-04",
-//     "偶像歌手": "icon-05",
-//     "煮饭仙人": "icon-06",
-//     "古玩大亨": "icon-07",
-//     "杰克国王": "icon-08",
-//     "贤德昭仪": "icon-09",
-//     "名射击手": "icon-11",
-//     "尾灯少女": "icon-12",
-//     "屠龙勇士": "icon-13",
-//     "赤子仙童": "icon-14",
-//     "鸾舞佳人": "icon-15",
-//     "王牌地主": "icon-16",
-//     "月半半": "icon-17",
-//     "魔力娃娃": "icon-18",
-//     "雪豹战士": "icon-20",
-//     "沉甸甸": "icon-21",
-//     "节奏大师": "icon-22",
-//     "Carry猫": "icon-23",
-// }
-// // common.sleep_default_with_msg("目前G分不太够，先不购买600G分的贤德昭仪。等俩月卡都买完再开启")
-// locateItemAndTryBuyIt("贤德昭仪", makeupMap)
-//
-// function locateItemAndTryBuyIt(targetName, nameToIconTextMap) {
-//     iconText = nameToIconTextMap[targetName]
-//     let notOwnText = className("android.view.View").text("未拥有").findOne()
-//     let itemList = notOwnText.parent().parent().parent().child(1).child(0)
-//     for (let i = 0; i < itemList.childCount(); i++) {
-//         item = itemList.child(i)
-//
-//         let image_icon = item.child(item.childCount() - 2).child(0).text()
-//         if (image_icon !== iconText) {
-//             continue
-//         }
-//
-//         item.click()
-//         common.sleep_default_with_msg("已定位到【" + targetName + "】，并完成点击，等待一会确保状态显示完成")
-//
-//         let infoPanel = notOwnText.parent().parent().parent().parent().parent().child(1)
-//         let buyButton = infoPanel.child(1)
-//         if (buyButton.text() === "已拥有") {
-//             common.sleep_default_with_msg("已经拥有 " + targetName + " ，本次不尝试购买")
-//         } else {
-//             common.sleep_default_with_msg("尚未拥有 " + targetName + " ，即将尝试进行购买")
-//             common.click("点击购买 " + targetName, buyButton.bounds().centerX(), buyButton.bounds().centerY())
-//             // common.click("点击 取消", 350,1250)
-//             common.click("点击 确定", 735, 1250)
-//         }
-//
-//         // 找到目标后停止处理
-//         break
-//     }
-// }
-//
-// common.back()
-//
-// common.headline("穿戴妆容和装饰")
-// common.click("点击 [心悦猫咪] 区域", 525, 910)
-// common.sleep("加载很慢，多等一段时间", 25)
-// common.click("点击 [仓库] 按钮", 990, 815)
-//
-// common.click("点击 [装饰] 按钮", 790, 390)
-// locateItemAndTryUseIt("小橘子", decorationMap)
-//
-// common.click("点击 [妆容] 按钮", 290, 390)
-// locateItemAndTryUseIt("贤德昭仪", makeupMap)
-//
-// function locateItemAndTryUseIt(targetName, nameToIconTextMap) {
-//     iconText = nameToIconTextMap[targetName]
-//     let returnBtnView = className("android.view.View").text("返回按钮").findOne()
-//     let itemList = returnBtnView.parent().child(3).child(0).child(0)
-//     let found = false
-//     for (let i = 0; i < itemList.childCount(); i++) {
-//         item = itemList.child(i)
-//
-//         let imageView = item.child(0)
-//         // 一般图标是最后一个子元素
-//         let image_icon = imageView.child(imageView.childCount() - 1)
-//         if (image_icon.childCount() === 0) {
-//             // 有时候会是倒数第二个
-//             image_icon = imageView.child(imageView.childCount() - 2)
-//         }
-//         if (image_icon.child(0).text() !== iconText) {
-//             continue
-//         }
-//
-//         // 已穿戴|穿戴
-//         // 已装饰|装饰
-//         let statusBtn = item.child(1)
-//         let status = statusBtn.text()
-//         if (status === "穿戴" || status === "装饰") {
-//             // 点击穿戴/装饰
-//             statusBtn.click()
-//             common.sleep_default_with_msg("尚未" + status + targetName + "，点击 " + status + " 按钮进行" + status)
-//
-//             let useBtnView = returnBtnView.parent().child(4).child(0).child(1).child(2)
-//             common.click("点击" + status + targetName, useBtnView.bounds().centerX(), useBtnView.bounds().centerY())
-//
-//             let closeBtnView = useBtnView.parent().parent().child(0)
-//             common.click(status + "完成，点击右上角关闭按钮", closeBtnView.bounds().centerX(), closeBtnView.bounds().centerY())
-//         } else if (status === "到期续费"){
-//             statusBtn.click()
-//             common.sleep_default_with_msg(targetName + "已过期，点击 " + status + " 按钮进行续费")
-//
-//             let buyBtnView = returnBtnView.parent().child(4).child(0).child(1).child(2)
-//             common.click("点击 续期按钮 续期" + targetName, buyBtnView.bounds().centerX(), buyBtnView.bounds().centerY())
-//
-//             common.click("点击 确定 进行购买", 735, 1250)
-//
-//             common.click("点击 好的", 540, 1320)
-//
-//             let useBtnView = returnBtnView.parent().child(4).child(0).child(1).child(2)
-//             common.click("点击" + status + targetName, useBtnView.bounds().centerX(), useBtnView.bounds().centerY())
-//
-//             let closeBtnView = useBtnView.parent().parent().child(0)
-//             common.click(status + "完成，点击右上角关闭按钮", closeBtnView.bounds().centerX(), closeBtnView.bounds().centerY())
-//         } else {
-//             common.sleep_default_with_msg(status + targetName + "，本次无需额外操作~")
-//         }
-//
-//         // 找到目标后停止处理
-//         found = true
-//         break
-//     }
-//
-//     if (!found) {
-//         common.sleep_default_with_msg("未能找到 " + targetName + "，应该是尚未购买~")
-//     }
-// }
-//
-// common.back()
-//
-// common.headline("战斗与历练")
-// common.click("点击 [心悦猫咪] 区域", 525, 910)
-// common.sleep("加载很慢，多等一段时间", 25)
-// let infoListView = className("android.widget.ListView").depth(11).rowCount(3).findOne()
-// let gPoints = parseInt(infoListView.child(0).text(), 10)
-// let fightingCapacity = parseInt(infoListView.child(1).text(), 10)
-// let yuanQi = parseInt(infoListView.child(2).text(), 10)
-//
-// let totalFightCount = Math.floor(yuanQi / 20)
-// common.headline("将进行战斗次数为：" + totalFightCount)
-// if (totalFightCount > 0) {
-//     let fightUsingTime = 15
-//
-//     common.click("点击 [战斗] 按钮，触发首次战斗", 420, 1725)
-//     common.sleep("等待猫咪战斗结束", fightUsingTime)
-//
-//     for (let i = 2; i <= totalFightCount; i++) {
-//         common.click("点击 [我还能打] 按钮，触发第" + i + "次战斗", 710, 1350)
-//         common.sleep("等待猫咪战斗结束", fightUsingTime)
-//     }
-//
-//     common.click("点击 [不了怕了] 按钮", 370, 1350)
-// } else {
-//     common.sleep_default_with_msg("元气不足20，无法进行战斗")
-// }
-//
-// common.headline("历练")
-// if (fightingCapacity >= 15) {
-//     common.click("点击 [历练] 按钮", 170, 1725)
-//     levels = [
-//         {stage: 1, x: 850, y: 1750},
-//         {stage: 2, x: 250, y: 1200},
-//         {stage: 3, x: 875, y: 900},
-//         {stage: 4, x: 500, y: 575},
-//     ]
-//     stage = 1
-//     if (15 <= fightingCapacity && fightingCapacity < 55) {
-//         stage = 1
-//     } else if (55 <= fightingCapacity && fightingCapacity < 90) {
-//         stage = 2
-//     } else if (90 <= fightingCapacity && fightingCapacity < 130) {
-//         stage = 3
-//     } else if (130 <= fightingCapacity) {
-//         stage = 4
-//     }
-//     common.sleep_default_with_msg("当前战力为 " + fightingCapacity + ", 最高可打第" + stage + "关，将依次尝试该关一直到第二关，直到找到一个可以进行的关卡")
-//     do {
-//         let levelPos = levels[stage - 1]
-//         common.click("点击 [第" + levelPos.stage + "关] 按钮", levelPos.x, levelPos.y)
-//         common.click("点击 [去吧] 按钮 或 当该关卡无剩余次数时出现的 [好的] 按钮", 700, 1500)
-//         common.click("若提示关卡已经猫满为患，点击 [好的] 按钮", 700, 1320)
-//
-//         stage -= 1
-//     } while (stage >= 2)
-// } else {
-//     common.sleep_default_with_msg("战力不足15，无法进行历练~")
-// }
-// common.back()
+common.headline("心悦猫咪")
+
+common.headline("领取历练奖励")
+common.click("点击 [心悦猫咪] 区域", 525, 910)
+common.sleep("加载很慢，多等一段时间", 25)
+common.click("点击 [领取礼包] 按钮", 170, 1725)
+common.click("点击 [好的] 按钮", 550, 1380)
+common.back()
+
+common.headline("尝试购买妆容和装饰")
+common.click("点击 [心悦猫咪] 区域", 525, 910)
+common.sleep("加载很慢，多等一段时间", 25)
+common.click("点击 [商店] 按钮", 910, 1725)
+
+// 小橘子的七日收益率较高，故而先尝试购买小橘子
+// 成本    300
+// 收益    30*2*7=420
+// 净利润  120
+// 收益率  120/300 = 40%
+common.click("点击 [装饰] 按钮", 790, 390)
+decorationMap = {
+    "喷香猫粮": "icon-00",
+    "飞车酷比": "icon-01",
+    "飞车墨汁": "icon-02",
+    "炫舞粉66": "icon-03",
+    "炫舞酷66": "icon-04",
+    "云裳茶壶": "icon-05",
+    "云裳童心饰": "icon-06",
+    "小橘子": "icon-07",
+}
+// common.sleep_default_with_msg("目前G分不太够，先不购买300G分的小橘子。等俩月卡都买完再开启")
+locateItemAndTryBuyIt("小橘子", decorationMap)
+
+// 贤德昭仪的七日收益率较低，故而在后面买
+// 成本    600
+// 收益    50*2*7=700
+// 净利润  100
+// 收益率  100/600 = 16.66%
+common.click("点击 [妆容] 按钮", 290, 390)
+makeupMap = {
+    "富贵士绅": "icon-01",
+    "加班女仆": "icon-02",
+    "浪子刀客": "icon-03",
+    "世家公子": "icon-04",
+    "偶像歌手": "icon-05",
+    "煮饭仙人": "icon-06",
+    "古玩大亨": "icon-07",
+    "杰克国王": "icon-08",
+    "贤德昭仪": "icon-09",
+    "名射击手": "icon-11",
+    "尾灯少女": "icon-12",
+    "屠龙勇士": "icon-13",
+    "赤子仙童": "icon-14",
+    "鸾舞佳人": "icon-15",
+    "王牌地主": "icon-16",
+    "月半半": "icon-17",
+    "魔力娃娃": "icon-18",
+    "雪豹战士": "icon-20",
+    "沉甸甸": "icon-21",
+    "节奏大师": "icon-22",
+    "Carry猫": "icon-23",
+}
+// common.sleep_default_with_msg("目前G分不太够，先不购买600G分的贤德昭仪。等俩月卡都买完再开启")
+locateItemAndTryBuyIt("贤德昭仪", makeupMap)
+
+function locateItemAndTryBuyIt(targetName, nameToIconTextMap) {
+    iconText = nameToIconTextMap[targetName]
+    let notOwnText = className("android.view.View").text("未拥有").findOne()
+    let itemList = notOwnText.parent().parent().parent().child(1).child(0)
+    for (let i = 0; i < itemList.childCount(); i++) {
+        item = itemList.child(i)
+
+        let image_icon = item.child(item.childCount() - 2).child(0).text()
+        if (image_icon !== iconText) {
+            continue
+        }
+
+        item.click()
+        common.sleep_default_with_msg("已定位到【" + targetName + "】，并完成点击，等待一会确保状态显示完成")
+
+        let infoPanel = notOwnText.parent().parent().parent().parent().parent().child(1)
+        let buyButton = infoPanel.child(1)
+        if (buyButton.text() === "已拥有") {
+            common.sleep_default_with_msg("已经拥有 " + targetName + " ，本次不尝试购买")
+        } else {
+            common.sleep_default_with_msg("尚未拥有 " + targetName + " ，即将尝试进行购买")
+            common.click("点击购买 " + targetName, buyButton.bounds().centerX(), buyButton.bounds().centerY())
+            // common.click("点击 取消", 350,1250)
+            common.click("点击 确定", 735, 1250)
+        }
+
+        // 找到目标后停止处理
+        break
+    }
+}
+
+common.back()
+
+common.headline("穿戴妆容和装饰")
+common.click("点击 [心悦猫咪] 区域", 525, 910)
+common.sleep("加载很慢，多等一段时间", 25)
+common.click("点击 [仓库] 按钮", 990, 815)
+
+common.click("点击 [装饰] 按钮", 790, 390)
+locateItemAndTryUseIt("小橘子", decorationMap)
+
+common.click("点击 [妆容] 按钮", 290, 390)
+locateItemAndTryUseIt("贤德昭仪", makeupMap)
+
+function locateItemAndTryUseIt(targetName, nameToIconTextMap) {
+    iconText = nameToIconTextMap[targetName]
+    let returnBtnView = className("android.view.View").text("返回按钮").findOne()
+    let itemList = returnBtnView.parent().child(3).child(0).child(0)
+    let found = false
+    for (let i = 0; i < itemList.childCount(); i++) {
+        item = itemList.child(i)
+
+        let imageView = item.child(0)
+        // 一般图标是最后一个子元素
+        let image_icon = imageView.child(imageView.childCount() - 1)
+        if (image_icon.childCount() === 0) {
+            // 有时候会是倒数第二个
+            image_icon = imageView.child(imageView.childCount() - 2)
+        }
+        if (image_icon.child(0).text() !== iconText) {
+            continue
+        }
+
+        // 已穿戴|穿戴
+        // 已装饰|装饰
+        let statusBtn = item.child(1)
+        let status = statusBtn.text()
+        if (status === "穿戴" || status === "装饰") {
+            // 点击穿戴/装饰
+            statusBtn.click()
+            common.sleep_default_with_msg("尚未" + status + targetName + "，点击 " + status + " 按钮进行" + status)
+
+            let useBtnView = returnBtnView.parent().child(4).child(0).child(1).child(2)
+            common.click("点击" + status + targetName, useBtnView.bounds().centerX(), useBtnView.bounds().centerY())
+
+            let closeBtnView = useBtnView.parent().parent().child(0)
+            common.click(status + "完成，点击右上角关闭按钮", closeBtnView.bounds().centerX(), closeBtnView.bounds().centerY())
+        } else if (status === "到期续费"){
+            statusBtn.click()
+            common.sleep_default_with_msg(targetName + "已过期，点击 " + status + " 按钮进行续费")
+
+            let buyBtnView = returnBtnView.parent().child(4).child(0).child(1).child(2)
+            common.click("点击 续期按钮 续期" + targetName, buyBtnView.bounds().centerX(), buyBtnView.bounds().centerY())
+
+            common.click("点击 确定 进行购买", 735, 1250)
+
+            common.click("点击 好的", 540, 1320)
+
+            let useBtnView = returnBtnView.parent().child(4).child(0).child(1).child(2)
+            common.click("点击" + status + targetName, useBtnView.bounds().centerX(), useBtnView.bounds().centerY())
+
+            let closeBtnView = useBtnView.parent().parent().child(0)
+            common.click(status + "完成，点击右上角关闭按钮", closeBtnView.bounds().centerX(), closeBtnView.bounds().centerY())
+        } else {
+            common.sleep_default_with_msg(status + targetName + "，本次无需额外操作~")
+        }
+
+        // 找到目标后停止处理
+        found = true
+        break
+    }
+
+    if (!found) {
+        common.sleep_default_with_msg("未能找到 " + targetName + "，应该是尚未购买~")
+    }
+}
+
+common.back()
+
+common.headline("战斗与历练")
+common.click("点击 [心悦猫咪] 区域", 525, 910)
+common.sleep("加载很慢，多等一段时间", 25)
+let infoListView = className("android.widget.ListView").depth(11).rowCount(3).findOne()
+let gPoints = parseInt(infoListView.child(0).text(), 10)
+let fightingCapacity = parseInt(infoListView.child(1).text(), 10)
+let yuanQi = parseInt(infoListView.child(2).text(), 10)
+
+let totalFightCount = Math.floor(yuanQi / 20)
+common.headline("将进行战斗次数为：" + totalFightCount)
+if (totalFightCount > 0) {
+    let fightUsingTime = 15
+
+    common.click("点击 [战斗] 按钮，触发首次战斗", 420, 1725)
+    common.sleep("等待猫咪战斗结束", fightUsingTime)
+
+    for (let i = 2; i <= totalFightCount; i++) {
+        common.click("点击 [我还能打] 按钮，触发第" + i + "次战斗", 710, 1350)
+        common.sleep("等待猫咪战斗结束", fightUsingTime)
+    }
+
+    common.click("点击 [不了怕了] 按钮", 370, 1350)
+} else {
+    common.sleep_default_with_msg("元气不足20，无法进行战斗")
+}
+
+common.headline("历练")
+if (fightingCapacity >= 15) {
+    common.click("点击 [历练] 按钮", 170, 1725)
+    levels = [
+        {stage: 1, x: 850, y: 1750},
+        {stage: 2, x: 250, y: 1200},
+        {stage: 3, x: 875, y: 900},
+        {stage: 4, x: 500, y: 575},
+    ]
+    stage = 1
+    if (15 <= fightingCapacity && fightingCapacity < 55) {
+        stage = 1
+    } else if (55 <= fightingCapacity && fightingCapacity < 90) {
+        stage = 2
+    } else if (90 <= fightingCapacity && fightingCapacity < 130) {
+        stage = 3
+    } else if (130 <= fightingCapacity) {
+        stage = 4
+    }
+    common.sleep_default_with_msg("当前战力为 " + fightingCapacity + ", 最高可打第" + stage + "关，将依次尝试该关一直到第二关，直到找到一个可以进行的关卡")
+    do {
+        let levelPos = levels[stage - 1]
+        common.click("点击 [第" + levelPos.stage + "关] 按钮", levelPos.x, levelPos.y)
+        common.click("点击 [去吧] 按钮 或 当该关卡无剩余次数时出现的 [好的] 按钮", 700, 1500)
+        common.click("若提示关卡已经猫满为患，点击 [好的] 按钮", 700, 1320)
+
+        stage -= 1
+    } while (stage >= 2)
+} else {
+    common.sleep_default_with_msg("战力不足15，无法进行历练~")
+}
+common.back()
 
 common.back()
 
