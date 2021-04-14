e96a8a09b466ed2f7548d5d53c7795d689364881
风之凌殇 <fzls.zju@gmail.com>
2021-04-14 19:44:14 +0800

    接入新的 预购活动

--- qq.js
+++ qq.js
@@ -19,18 +19,27 @@ common.sleep("搜索 [我的电脑]，需要耗费一些时间，等待若干时
 let myComputer = className("android.widget.TextView").text("我的电脑").findOne()
 common.click("点击 [我的电脑] 对话框", myComputer.bounds().centerX(), myComputer.bounds().centerY())
 
-let activityUrlTextView = className("android.widget.TextView").text("https://pay.qq.com/h5/active/pay_lottery_stage_dnf20210107.php").findOne()
+let activityUrlTextView = className("android.widget.TextView").text("https://pay.qq.com/h5/activity/dnf_dqgylb_lottery.php").findOne()
 common.click("点击 [预购活动链接] 聊天记录", activityUrlTextView.bounds().centerX(), activityUrlTextView.bounds().centerY())
 
-common.swipe("1/2 上滑直至[抽奖界面]界面完全展现", 540, 2000, 540, 300, 1000)
-common.swipe("2/2 上滑直至[抽奖界面]界面完全展现", 540, 2000, 540, 300, 1000)
+common.swipe("1/2 上滑直至[领取抽奖券]界面完全展现", 540, 2000, 540, 300, 1000)
+common.swipe("2/2 上滑直至[领取抽奖券]界面完全展现", 540, 2000, 540, 300, 1000)
 
+let takeDailyLoginTicketBtn = className("android.view.View").text("每日登录抽奖券×1").findOne().parent().parent().child(2)
+common.click("点击 [立即领取] 每日登录抽奖券×1", takeDailyLoginTicketBtn.bounds().centerX(), takeDailyLoginTicketBtn.bounds().centerY())
+common.click_text("点击 [确定]", "确定")
+
+let takeLuckyTicketBtn = className("android.view.View").text("幸运用户抽奖券×1").findOne().parent().parent().child(2)
+common.click("点击 [立即领取] 幸运用户抽奖券×1", takeLuckyTicketBtn.bounds().centerX(), takeLuckyTicketBtn.bounds().centerY())
+common.click_text("点击 [确定]", "确定")
+
+common.swipe("1/2 上滑直至[抽奖界面]界面完全展现", 540, 2000, 540, 300, 1000)
 
 // 为了保险起见，多点几次
 for (let i = 0; i < 3; i++) {
-    common.click("第" + (i+1) + "次点击 [立即开奖] 按钮", 550, 1050)
+    common.click("第" + (i+1) + "次点击 [立即开奖] 按钮", 550, 1100)
     common.sleep_default_with_msg("额外等待一会，确保抽奖动画完毕")
-    common.click("第" + (i+1) + "次点击 [关闭奖励展示] 按钮", 990, 845)
+    common.click("第" + (i+1) + "次点击 [关闭奖励展示] 按钮（可能需要调整）", 550, 1405)
 }
 
 common.back_to_top("跳回主界面", 3, 3000)
