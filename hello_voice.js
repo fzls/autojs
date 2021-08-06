1f8346a23b86637081d0c0da8663cbfc150d24b7
风之凌殇 <fzls.zju@gmail.com>
2021-08-06 20:48:45 +0800

    hello语音增加家族签到

--- hello_voice.js
+++ hello_voice.js
@@ -1,18 +1,19 @@
 let common = require("_common.js")
 
 common.init("hello语音dnf活动自动化脚本")
-
-common.reLaunchPackage("重新打开 hello语音", "com.yy.huanju")
-
-common.click("处理可能弹出的开屏活动页", 935, 420)
-common.click("处理首次登录会弹出的签到按钮", 540, 1425)
-common.back_to_top("按多次back，确保跳到最顶层", 5, 3000)
+//
+// common.reLaunchPackage("重新打开 hello语音", "com.yy.huanju")
+//
+// common.click("处理可能弹出的开屏活动页", 935, 420)
+// common.click("处理首次登录会弹出的签到按钮", 540, 1425)
+// common.back_to_top("按多次back，确保跳到最顶层", 5, 3000)
 
 common.headline("获取Hello贝")
 common.click("点击 [首页] Tab", 105, 2050)
 common.click("点击 [游戏] 分页", 78, 160)
-common.click_sleep_long("点击 [福利中心]", 310, 800)
-common.click("点击 [获取Hello贝]", 300, 670)
+common.click_sleep_long("点击 [游戏中心]", 200, 720)
+common.click_text("点击 [福利与活动]", "福利与活动")
+common.click_text("点击 [我的hello贝]", "我的hello贝")
 common.click("点击 登录DNF助手 右侧的 [去完成]", 930, 2100)
 common.sleep_default_with_msg("等待助手进入完毕")
 common.exit_current_app("退出DNF助手，回到Hello语音")
@@ -20,41 +21,13 @@ common.sleep_default_with_msg("等待助手退出完毕")
 common.click("点击 登录DNF助手 右侧的 [领取]", 930, 2100)
 common.back_to_top("返回首页", 2, 3000)
 
-// common.headline("进入活动界面")
-// common.click("点击 [首页] Tab", 105, 2050)
-// common.click("点击 [游戏] 分页", 78, 160)
-// common.click_sleep_long("点击 [福利中心]", 310, 800)
-// common.click("点击 [使用Hello贝]", 800, 670)
-//
-// common.headline("Hello助力通关黑鸦")
-// common.click_sleep_long("点击 [Hello助力通关黑鸦]", 530, 1600)
-// common.swipe("上滑直至[进阶奖励]处于页面最上方", 550, 1950, 550, 70, 1000)
-//
-// common.click("点击 [进阶奖励]", 270, 315)
-// common.click("点击 [成团礼包]", 550, 950)
-// common.click("点击 [一级团]", 925, 1280)
-// common.click("点击 [确定]", 550, 1244)
-// common.click("点击 [二级团]", 925, 1640)
-// common.click("点击 [确定]", 550, 1244)
-// common.click("点击 [三级团]", 925, 2000)
-// common.click("点击 [确定]", 550, 1244)
-//
-// common.click("点击 [每日训练]", 810, 315)
-// common.click("点击 [签到]", 920, 600)
-// common.swipe("上滑直至[每日训练回馈抽奖] 相关内容完整出现在页面下方", 550, 1950, 550, 200, 1000)
-// for (let i = 0; i < 3; i++) {
-//     common.click_sleep_long("点击 [抽奖]", 550, 1000)
-//     common.click("点击 [确定] 以跳过抽奖成功提示", 755, 1330)
-//     common.click("点击 [确定] 以跳过抽奖次数不足提示", 540, 1270)
-// }
-// common.click("点击 [领取] 累积抽奖3次", 220, 1900)
-// common.click("点击 [确定]", 550, 1244)
-// common.click("点击 [领取] 累积抽奖7次", 545, 1900)
-// common.click("点击 [确定]", 550, 1244)
-// common.click("点击 [领取] 累积抽奖15次", 865, 1900)
-// common.click("点击 [确定]", 550, 1244)
-// common.back() // 回到上一层以方便后续操作
-//
-// common.back_to_top("返回首页", 2, 3000)
+common.headline("获取Hello贝")
+common.click("点击 [首页] Tab", 105, 2050)
+common.click("点击 [游戏] 分页", 78, 160)
+common.click_sleep_long("点击 [游戏中心]", 200, 720)
+common.click_text("点击 [DNF家族]", "DNF家族")
+common.click_text("点击 [回家]", "回家")
+common.click_sleep_long("点击 [签到]", 185, 1370)
+common.back_to_top("返回首页", 4, 3000)
 
 common.foot()
