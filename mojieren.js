8776687a0d53631ea0ba5cf97770e5406c5a68eb
风之凌殇 <fzls.zju@gmail.com>
2022-01-04 11:00:29 +0800

    feat: 增加打开chrome和调整提示语

--- mojieren.js
+++ mojieren.js
@@ -2,11 +2,13 @@ let common = require("_common.js")
 
 common.init("点击微信支付按钮来尝试购买魔界人粘土人")
 
+common.launchPackage("打开chrome", "com.android.chrome")
+
 const initDuration = 10
 const waitDuration = 5
 const payUrl = "https://dnfcity.qq.com/mobile/html/cart_pay.html####"
 
-common.sleep("请在 " + initDuration + " 秒内转到魔界人支付界面，以便脚本进行点击尝试。链接为  " + payUrl + "", initDuration)
+common.sleep("请在 " + initDuration + " 秒内打开魔界人支付界面（需要先在购物车页面完成登录），以便脚本进行点击尝试。链接为  " + payUrl + "", initDuration)
 
 let tryIndex = 0;
 while (true) {
